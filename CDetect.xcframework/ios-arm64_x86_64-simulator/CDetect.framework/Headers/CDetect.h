802021f7782f6e939ca813d59c8750dbdf4381d6
Ruaridh Wylie <99192322+Ruaridh-Waire-Health@users.noreply.github.com>
2023-10-12 13:53:15 +0100

    Add files via upload

--- CDetect.xcframework/ios-arm64_x86_64-simulator/CDetect.framework/Headers/CDetect.h
+++ CDetect.xcframework/ios-arm64_x86_64-simulator/CDetect.framework/Headers/CDetect.h
@@ -14,6 +14,3 @@
 //FOUNDATION_EXPORT const unsigned char CDetectVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CDetect/PublicHeader.h>
-
-FOUNDATION_EXPORT double TESTVAL;
-
